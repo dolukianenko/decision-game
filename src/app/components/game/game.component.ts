fa1b2a2181730541d9ff5aa5fcf929baa1026ec5
dolukianenko <dolukianenko@gmail.com>
2024-12-12 11:43:57 +0100

    Implement basic question flow: Game component fetches first question, passes to Question component, and handles answer selection

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
 import { QuestionComponent } from '../question/question.component';
 import { DiagramComponent } from '../diagram/diagram.component';
+import { Answer, Question } from '../../models/question.model';
+import { QuestionService } from '../../services/question.service';
 
 @Component({
   selector: 'app-game',
@@ -9,4 +12,34 @@ import { DiagramComponent } from '../diagram/diagram.component';
   templateUrl: './game.component.html',
   styleUrl: './game.component.css'
 })
-export class GameComponent { }
+export class GameComponent {
+  private destroy$ = new Subject<void>();
+  currentQuestion: Question | undefined;
+
+  constructor(private questionService: QuestionService) { }
+
+  ngOnInit(): void {
+    this.getStartingQuestion();
+  }
+
+  getStartingQuestion(): void {
+    this.questionService.getFirstQuestion()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((firstQuestion) => this.currentQuestion = firstQuestion);
+  }
+
+  handleAnswerClick(answer: Answer) {
+    this.getNextQuestion(answer.nextQuestionId);
+  }
+
+  getNextQuestion(id: string) {
+    this.questionService.getQuestionById(id)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((question) => this.currentQuestion = question);
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+}
--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { Answer, Question } from '../../models/question.model';
 
 @Component({
   selector: 'app-question',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatButtonModule],
   templateUrl: './question.component.html',
   styleUrl: './question.component.css'
 })
-export class QuestionComponent { }
+export class QuestionComponent {
+  @Input() question?: Question;
+  @Output() answerSelected: EventEmitter<Answer> = new EventEmitter();
+
+  onAnswerClick(answer: Answer): void {
+    this.answerSelected.emit(answer);
+  }
+}
